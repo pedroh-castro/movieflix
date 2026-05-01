37f9b5777a2e6a4d725efd22c4332a8d18c211b7
Pedro Henrique <phcasttro@outlook.com>
2026-05-01 13:59:28 -0300

    feat: repository category

---
+++ src/main/java/com/phc/movieflix/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.phc.movieflix.repository;
+
+import com.phc.movieflix.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
