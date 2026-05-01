080d55cd4459f093e31b1e8e84bc8a68edb1e641
Pedro Henrique <phcasttro@outlook.com>
2026-05-01 11:25:35 -0300

    first commit

---
+++ src/test/java/com/phc/movieflix/MovieflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phc.movieflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
